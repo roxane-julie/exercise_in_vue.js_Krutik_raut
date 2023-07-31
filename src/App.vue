be91ac8dbf830f91465c5b32b2068e2e705a7d35
Roxane Breton <julie.roxane@outlook.fr>
2023-07-31 15:21:43 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <Header/>
+</template>
+
+
+<script setup>
+import Header from "../src/components/Header.vue"
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <p>Header</p>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
